3f95be174f6a464cb43e2cd6c600b248159d43aa
Laptop-Sjors <sjorspnk@gmail.com>
2022-02-10 16:04:16 +0100

    Initial Commit

---
+++ Source/UEAnimalCareRemake/UEAnimalCareRemakeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UEAnimalCareRemakeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UEANIMALCAREREMAKE_API AUEAnimalCareRemakeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
